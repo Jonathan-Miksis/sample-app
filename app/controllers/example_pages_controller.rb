35b67f55f50bd83665c2ec704cd066dbad40a751
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-10 11:26:03 -0400

    add route test file and new url landing page

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -1,4 +1,10 @@
 class ExamplePagesController < ApplicationController
   def hello_action
     render json: {message: "This is so cool"}
+  end
+
+  def tv_action
+    render json: {message: "You should watch The Americans on FX"}
+  end
+  
 end
